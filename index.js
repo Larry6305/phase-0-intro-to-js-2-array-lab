b042c606ed516884dc5f97c3fe40e7ad238408b6
Larry <melarry46@gmail.com>
2025-03-11 10:07:00 +0300

    completed js arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution here!
+// Initial array of cats
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Function to append a cat to the end of the array destructively
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+module.exports = { cats, destructivelyAppendCat };
+
+// Initial array of cats
+let Cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive functions
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = { 
+  cats, 
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat 
+};
